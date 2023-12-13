dc8173b0c7df591d6e6e1ae68f4c383e19cdccc2
MatiCufr3 <matiasjogonzalez@gmail.com>
2023-12-13 15:56:00 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,7 @@
+const libros = [
+   { id: '1', titulo: 'LA 3', autor: 'La Scaloneta' },
+   { id: '2', titulo: 'QUE MIRAS BOBO', autor: 'Lionel Messi' },
+   { id: '3', titulo: 'CIEN AÑOS DE SOLEDAD', autor: 'Gabriel Garcia Marquez' }
+ ];
+ module.exports = libros;
+    
